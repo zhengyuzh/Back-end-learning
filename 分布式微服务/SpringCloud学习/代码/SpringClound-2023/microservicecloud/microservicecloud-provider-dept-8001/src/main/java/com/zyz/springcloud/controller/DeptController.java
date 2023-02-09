71a4b41098a5f95e50c4dbb34888dfd3504d9b94
18348375641 <a11083879A>
2023-02-09 13:15:08 +0800

    1、api模块和部门Entity实体类的创建
    2、部门服务提供者，子模块的启动类要自己定义。子模块引入公共模块的实体类方式

---
+++ 分布式微服务/SpringCloud学习/代码/SpringClound-2023/microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/zyz/springcloud/controller/DeptController.java
@@ -0,0 +1,39 @@
+package com.zyz.springcloud.controller;
+
+import com.zyz.springcloud.entities.Dept;
+import com.zyz.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class DeptController
+{
+	@Autowired
+	private DeptService service;
+
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	public boolean add(@RequestBody Dept dept)
+	{
+		return service.add(dept);
+	}
+
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	public Dept get(@PathVariable("id") Long id)
+	{
+		return service.get(id);
+	}
+
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list()
+	{
+		return service.list();
+	}
+
+	
+
+
+}
