f74c8a9459c0bb988d136aff9b21a3b2e9f6e84b
18348375641 <a11083879A>
2023-02-15 08:42:55 +0800

    将已有的部门微服务注册进Eureka服务中心

--- 分布式微服务/SpringCloud学习/代码/SpringClound-2023/microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/zyz/springcloud/DeptProvider8001_App.java
+++ 分布式微服务/SpringCloud学习/代码/SpringClound-2023/microservicecloud/microservicecloud-provider-dept-8001/src/main/java/com/zyz/springcloud/DeptProvider8001_App.java
@@ -2,9 +2,12 @@ package com.zyz.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+
 public class DeptProvider8001_App
 {
 	public static void main(String[] args)
