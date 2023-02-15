fa27d049df601efab0bcd0ee9e832edf50454239
18348375641 <a11083879A>
2023-02-15 09:08:21 +0800

    初始化一个springboot项目用来测试native的相关代码【2023-02-15】

---
+++ 框架学习/SpringBoot框架/代码/mynative/src/main/java/com/zyz/mynative/MynativeApplication.java
@@ -0,0 +1,13 @@
+package com.zyz.mynative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MynativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MynativeApplication.class, args);
+    }
+
+}
---
+++ 框架学习/SpringBoot框架/代码/mynative/src/test/java/com/zyz/mynative/MynativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyz.mynative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
