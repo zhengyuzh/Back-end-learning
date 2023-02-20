ea741e3cee0d30121e767b1e6ccd41e438ea42d9
18348375641 <a11083879A>
2023-02-20 23:08:15 +0800

    服务降级 【2023-02-20】

--- 分布式微服务/SpringCloud学习/代码/SpringClound-2023/microservicecloud/microservicecloud-api/src/main/java/com/zyz/springcloud/service/DeptClientService.java
+++ 分布式微服务/SpringCloud学习/代码/SpringClound-2023/microservicecloud/microservicecloud-api/src/main/java/com/zyz/springcloud/service/DeptClientService.java
@@ -13,9 +13,12 @@ import java.util.List;
  * @version 1.0
  * @data 2023/2/20 21:04
  * @Description:修改microservicecloud-api工程，根据已经有的DeptClientService接口
+ * 新建
+ *
+ * 一个实现了FallbackFactory接口的类DeptClientServiceFallbackFactory
  */
-@FeignClient(value = "MICROSERVICECLOUD-DEPT")
-//@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
+//@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+@FeignClient(value = "MICROSERVICECLOUD-DEPT",fallbackFactory=DeptClientServiceFallbackFactory.class)
 public interface DeptClientService
 {
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
