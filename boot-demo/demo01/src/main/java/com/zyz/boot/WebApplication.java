cf74115aa376b3513425dcbda925f97efd093e15
18348375641 <a11083879A>
2023-08-28 11:31:04 +0800

    【1】SpringBoot 整合 Thymeleaf 模板;  maven工程改造成为SpringBoot项目 ; 整合Thymeleaf 的相关配置

---
+++ boot-demo/demo01/src/main/java/com/zyz/boot/WebApplication.java
@@ -0,0 +1,17 @@
+package com.zyz.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zyz
+ * @version 1.0
+ * @data 2023/8/28 10:53
+ * @Description: 启动类
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class,args);
+    }
+}
