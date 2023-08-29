56238297595408a043243f0e67c70286a5fd9f2f
18348375641 <a11083879A>
2023-08-29 11:10:49 +0800

    【5】 基础语法 th:text、获取值; th:object、获取对象 ;th:each，遍历列表数据到页面

--- boot-demo/demo01/src/main/java/com/zyz/boot/web/controller/UserController.java
+++ boot-demo/demo01/src/main/java/com/zyz/boot/web/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.zyz.boot.web.controller;
 
 
+import com.zyz.boot.web.pojo.School;
 import com.zyz.boot.web.pojo.User;
 import com.zyz.boot.web.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -18,7 +21,7 @@ import java.util.List;
  * @Description:
  */
 @Slf4j
-@RestController
+@Controller
 @RequestMapping("/system/user")
 public class UserController {
 
@@ -33,5 +36,40 @@ public class UserController {
         List<User> userList = userService.queryUser(user);
         System.out.println(userList.toString());
 
+    }
+
+    @RequestMapping(value = "/getOneObject", method = RequestMethod.GET)
+    public String toObject(ModelMap map){
+        User user = new User();
+        user.setUserId(1L);
+        user.setUserName("张三");
+        user.setEmail("zs@qq.com");
+        user.setPassword("123456");
+        map.addAttribute("user",user);
+        return "index";
+    }
+
+    @RequestMapping("/toSchool")
+    public String toAddUser(ModelMap map){
+        School school = new School();
+        school.setName("清华大学");
+        school.setAddress("北京");
+        map.addAttribute("school",school);
+        return "addSchool";
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public String saveUser(@ModelAttribute School school,ModelMap map){
+        System.out.println(school.toString());
+        map.addAttribute("school",school);
+        return "addSchool";
+    }
+
+    @RequestMapping(value = "/queryUserList", method = RequestMethod.GET)
+    public String userList(ModelMap model){
+        User user = new User();
+        List<User> userList = userService.queryUser(user);
+        model.addAttribute("userList",userList);
+        return "userList";
     }
 }
---
+++ boot-demo/demo01/src/main/java/com/zyz/boot/web/pojo/School.java
@@ -0,0 +1,18 @@
+package com.zyz.boot.web.pojo;
+
+import lombok.Data;
+
+/**
+ * @author zyz
+ * @version 1.0
+ * @data 2023/8/29 9:57
+ * @Description:
+ */
+@Data
+public class School {
+    /** 学校名称 */
+    private String name;
+
+    /** 学校地址 */
+    private String address;
+}
